df19f1b194b2670a7f63951295582b34f3435762
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-19 13:24:53 +0530

    first commit

--- login-system/server.js
+++ login-system/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
-
+const path = require("node:path");
 const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+//load a static file
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 // home route
 app.get("/", (req, res) => {
-  res.render();
+  res.render("base", { title: "login system" });
+});
+
+app.listen(port, () => {
+  console.log("server started on port", port);
 });
